a8bb7d215fe2b540872be65e908efffd7f261a47
Kyllian Marie-Magdelaine <77273553+kylyon@users.noreply.github.com>
2023-04-11 17:05:19 +0200

    Kyllian AI Début

---
+++ 5A_ESGI_AI_BehaviorTree_AIImplementation-master/Kyllian_BT.cs
@@ -0,0 +1,13 @@
+﻿using AI_BehaviorTree_AIGameUtility;
+using BehaviorTree_ESGI;
+
+public class ShootAction : Node
+{
+    public ShootAction() { }
+
+    override
+    public void Execute()
+    {
+        new AIActionFire();
+    }
+}
