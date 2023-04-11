c11a417ec5f125cd44bbb5adb63eccf5bfc31970
Talyj <julientay03@gmail.com>
2023-04-11 16:15:53 +0200

    ouais ?

---
+++ 5A_ESGI_AI_BehaviorTree_AIImplementation-master/BehaviorTree.cs
@@ -0,0 +1,63 @@
+﻿using AI_BehaviorTree_AIGameUtility;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+public class Sequence : Node
+{
+    public List<Node> nodes;
+
+    public Sequence()
+    {
+        nodes = new List<Node>();
+    }
+
+    public void Execute()
+    {
+        foreach (Node n in nodes)
+        {
+            if (n.state == NodeState.Failure)
+            {
+                this.state = NodeState.Failure;
+            }
+
+
+        }
+    }
+}
+
+public class Selector : Node
+{
+
+}
+
+public class Condition : Node
+{
+    public Condition()
+    {
+
+    }
+}
+
+public class Action : Node
+{
+    public Action()
+    {
+
+    }
+}
+
+public enum NodeState
+{
+    NotExecuted = 0, Failure = 1, Success = 2, Running = 4
+}
+
+public class Node
+{
+    public NodeState state;
+    public Node()
+    {
+        state = NodeState.NotExecuted;
+    }
+
+}
---
+++ 5A_ESGI_AI_BehaviorTree_AIImplementation-master/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_BehaviorTree_AIImplementation_0
+{
+    internal class Node
+    {
+        public enum currentState
+        {
+            NotExecuted,
+            Running,
+            Failure,
+            Success
+        }
+
+        public enum type
+        {
+            NotExecuted,
+            Running,
+            Failure,
+            Success
+        }
+
+        public 
+        public currentState state;
+        public List<Node> nodes = new List<Node>();
+
+        public Node()
+        {
+            state = currentState.NotExecuted;
+        }
+    }
+}
